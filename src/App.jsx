276738b7055c9567b9d9743544612268c092bf2f
Niang Codeur <niangpro2405@gamil.com>
2025-06-17 19:37:54 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-cyan-700 text-white'>je suis le meilleur developpeur react au monde</h1>
+    </div>
+  )
+}
+
+export default App
